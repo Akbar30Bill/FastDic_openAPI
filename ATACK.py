1b700794975421cb3f20c446587aa86d2ce8e57a
Akbar30Bill <alishahverdi2010@yahoo.com>
2018-10-16 22:57:52 +0330

    up

--- ATACK.py
+++ ATACK.py
@@ -16,17 +16,18 @@ db = db.cursor()
 try:
     for word_id in range(_from , _to):
         try:
+            print(word_id + ": " , end="")
             db.execute(f"SELECT * FROM english_words where english_word_id={word_id}")
             english_word = db.fetchone()
             english_word = re.search('\'(.*)\'' , str(english_word)).group(1)
             translated = fastDic.find_meaning(english_word)
             str_to_file = str(english_word) + ": " + str(translated) + "\n"
-            print (word_id + ": " + str_to_file)
+            print (str_to_file)
             file.write(str_to_file)
         except KeyboardInterrupt:
             file.close()
             print("KeyboardInterrupt")
-            sys.exit(0)
+            sys.exitx(0)
         except :
             pass
 except KeyboardInterrupt:
