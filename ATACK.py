84a523b48a409ede219eb4ce21fe0929b9f0f226
Akbar30Bill <alishahverdi2010@yahoo.com>
2018-10-16 22:55:19 +0330

    up

--- ATACK.py
+++ ATACK.py
@@ -3,10 +3,10 @@ import fastDic
 import sys
 import re
 
-_from = int(sys.argv[2])
-_to = int(sys.argv[3])
+_from = 1
+_to = 93000
 db_file = sys.argv[1]
-outfile = sys.argv[4]
+outfile = sys.argv[2]
 
 file = open(outfile , "w+")
 
@@ -15,13 +15,20 @@ db = db.cursor()
 
 try:
     for word_id in range(_from , _to):
-        db.execute(f"SELECT * FROM english_words where english_word_id={word_id}")
-        english_word = db.fetchone()
-        english_word = re.search('\'(.*)\'' , str(english_word)).group(1)
-        translated = fastDic.find_meaning(english_word)
-        str_to_file = str(english_word) + ": " + str(translated) + "\n"
-        print (str_to_file)
-        file.write(str_to_file)
+        try:
+            db.execute(f"SELECT * FROM english_words where english_word_id={word_id}")
+            english_word = db.fetchone()
+            english_word = re.search('\'(.*)\'' , str(english_word)).group(1)
+            translated = fastDic.find_meaning(english_word)
+            str_to_file = str(english_word) + ": " + str(translated) + "\n"
+            print (word_id + ": " + str_to_file)
+            file.write(str_to_file)
+        except KeyboardInterrupt:
+            file.close()
+            print("KeyboardInterrupt")
+            sys.exit(0)
+        except :
+            pass
 except KeyboardInterrupt:
     file.close()
     print("KeyboardInterrupt")
